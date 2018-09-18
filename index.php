326577cb4fc5e4a12ec0217ad79499e5eaf2a9b2
elmira2705 <37979137+elmira2705@users.noreply.github.com>
2018-09-18 16:23:41 +0600

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     ini_set('display_errors', 1);
 
     require_once 'autoload.php';
+    require_once __DIR__ . '/model/Connect.php';
 
     $route = new Route('controller/');
     $route -> start();
